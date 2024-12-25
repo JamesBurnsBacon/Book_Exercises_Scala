c06abfa44160346f40f985cff290854abfe29e03
James Burns <65990764+JamesBurnsBacon@users.noreply.github.com>
2024-12-25 11:32:09 -0500

    Ch 3.1 complete

--- .scala-build/Scala_For_The_Impatient_05338cf2ac/src_generated/main/scala-impatient-3e-code/ch3/src/main/scala/section1.worksheet.scala
+++ .scala-build/Scala_For_The_Impatient_05338cf2ac/src_generated/main/scala-impatient-3e-code/ch3/src/main/scala/section1.worksheet.scala
@@ -12,11 +12,20 @@ val moreStrings = Array.ofDim[String](5)
 val nums = Array.ofDim[Int](10)
   // An array of ten integers, all initialized with zero
 
+
+strings
 // Use () to access elements
 strings(0) = "Goodbye"
   // strings is now Array("Goodbye", "World")
 strings
-strings
+
+
+// Array if len is fixed, ArrayBuffer if len varies
+// () to access elements
+// to traverse, use                for elem <-arr do
+// to transform into new array     for elem <-arr yield
+// sc and java arrays are interoperable, for ArrayBuffer, ues scala.jdk.CollectionConverters
+
 
 /*</script>*/ /*<generated>*//*</generated>*/
 }
